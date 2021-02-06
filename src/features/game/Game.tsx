81a9019b046083b51f9c22ae8ada6a55a05acccb
Sergey Demidov <sergey.k.demidov@gmail.com>
2021-02-06 16:39:23 +0300

    Add levels

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -3,10 +3,10 @@ import { useHistory } from 'react-router-dom';
 
 export const Game: FC = () => {
   const history = useHistory();
-
+  const canvas = React.createRef<HTMLCanvasElement>();
   return (
     <div className="Game">
-      Game page
+      <canvas ref={canvas} />
       <br />
       <button onClick={() => history.goBack()}>Go back</button>
     </div>
--- src/features/game/constants.ts
+++ src/features/game/constants.ts
@@ -6,3 +6,9 @@ export enum ballColors {
   PINK,
   GREY
 }
+
+export const frame = Object.freeze({
+  width: 800,
+  height: 600,
+  rafe: 24,
+});
