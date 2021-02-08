016ecee78b3052a674ff939750c107534519695c
Sergey Demidov <sergey.k.demidov@gmail.com>
2021-02-08 20:24:45 +0300

    fix eslint linebreak-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,8 @@ module.exports = {
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/typedef": "off",
     "@typescript-eslint/no-unnecessary-type-arguments": "off",
+    'linebreak-style': 0,
+
 
     // TODO: Удалить эти правила, когда в @vkontakte/eslint-config обновят @typescript-eslint/eslint-plugin и @typescript-eslint/parser
     "@typescript-eslint/camelcase": "off",
