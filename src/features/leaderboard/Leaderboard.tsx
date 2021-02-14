8828609720d7212a252de274c1beb770029a8f2d
romalenskiy <kuperman88@gmail.com>
2021-02-14 23:21:58 +0300

    Use leaderboard top, leaderboard table and api methods

--- src/features/leaderboard/Leaderboard.tsx
+++ src/features/leaderboard/Leaderboard.tsx
@@ -1,20 +1,78 @@
-import React, { FC } from 'react';
-import { useHistory } from 'react-router-dom';
+import './leaderboard.css';
+import React, { FC, useEffect, useState } from 'react';
 import b_ from 'b_';
-import { useAuth } from '../../hooks';
+import { useAsyncAction, useAuth } from '../../hooks';
+import { LeaderboardTop } from './Components/Top/LeaderboardTop';
+import { Box, Heading } from 'grommet';
+import { getText } from '../../common/langUtils';
+import { useSelector } from 'react-redux';
+import { getCurrentUser } from '../../store/selectors';
+import { LeaderboardTable } from './Components/Table/LeaderboardTable';
+import { TOP_USERS_COUNT } from './constants';
+import { asyncAppActions } from '../../store/asyncActions';
+import { getLeaderboardRecords } from './selectors';
+import { Spinner } from '../../components/Spinner/Spinner';
+import { LeaderboardTablePlaceholder } from './Components/TablePlaceholder/LeaderboardTablePlaceholder';
 
 const block = b_.lock('leaderboard');
 
 export const Leaderboard: FC = () => {
   useAuth();
 
-  const history = useHistory();
+  const getLeaderboard = useAsyncAction(asyncAppActions.getLeaderboard);
+
+  const currentUser = useSelector(getCurrentUser);
+  const records = useSelector(getLeaderboardRecords);
+
+  const topUsers = records.slice(0, TOP_USERS_COUNT);
+  const needTopUsers = topUsers.length === TOP_USERS_COUNT;
+
+  const tableUsers = needTopUsers ? records.slice(TOP_USERS_COUNT) : records;
+  const tableStartPlace = needTopUsers ? TOP_USERS_COUNT : 0;
+  const needTable = Boolean(tableUsers.length);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // TODO: поддержать пагинацию
+    getLeaderboard({ cursor: 0, limit: 100 })
+      .finally(() => setIsLoading(false));
+  }, []);
 
   return (
     <div className={block()}>
-      Leaderboard
-      <br />
-      <button onClick={() => history.goBack()}>Go back</button>
+      <Heading textAlign="center" level="2">
+        {getText('leaderboard_page_header')}
+      </Heading>
+
+      {isLoading &&
+        <Box align="center">
+          <Spinner />
+        </Box>
+      }
+
+      {!isLoading &&
+        <>
+          {needTopUsers &&
+            <LeaderboardTop
+              records={topUsers}
+              currentUserId={currentUser.id}
+            />
+          }
+
+          {needTable &&
+            <div className={block('table-wrap')}>
+              <LeaderboardTable
+                records={tableUsers}
+                currentUserId={currentUser.id}
+                startPlace={tableStartPlace}
+              />
+            </div>
+          }
+
+          {!needTable && <LeaderboardTablePlaceholder />}
+        </>
+      }
     </div>
   );
 };
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -2,4 +2,6 @@ import { RootState } from './store';
 
 const getAppState = (state: RootState) => state.app;
 
+export const getCurrentUser = (state: RootState) => getAppState(state).user;
+
 export const getIsSignedInd = (state: RootState) => getAppState(state).isSignedIn;
