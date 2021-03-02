2a67500272cd8a3f9b8569b529fc0aa09cedbc31
Sergey Demidov <sergey.k.demidov@gmail.com>
2021-03-02 18:37:35 +0300

    Add sslify to express

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const path = require('path');
 const csp = require("./middlewares/csp");
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 const app = express();
 
 app.use(compression());
 
 app.use(csp.cspHeader)
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
