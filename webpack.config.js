9ed40a72579c7ff894eee9cac6e58b0eb6a8dada
Sergey Demidov <sergey.k.demidov@gmail.com>
2021-03-05 16:48:23 +0300

    Fix incorrect merging

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const isProductionMode = process.env.NODE_ENV === 'production';
 
@@ -71,7 +72,6 @@ module.exports = {
       template: './src/index.html',
       inject: 'head',
       scriptLoading: 'defer',
-      favicon: './src/favicon.ico',
     }),
     new MiniCssExtractPlugin({
       filename: 'index.css',
@@ -83,7 +83,14 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         { from: './src/pages/game/assets/fonts/Bangers.ttf' },
+        { from: './src/pwa/' },
       ],
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 10*1024*1024,
+      navigateFallback: '/index.html',
     }),
   ],
 }
