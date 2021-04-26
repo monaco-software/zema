4a727722c72009e84d56c5cc3c45009d709f7e7b
Sergey Demidov <sergey.k.demidov@gmail.com>
2021-04-26 19:36:16 +0300

    add caching strategy

--- server/ssr/ssrHandler.tsx
+++ server/ssr/ssrHandler.tsx
@@ -92,6 +92,7 @@ const Html: FC<Props> = ({ children, serverState }) => {
           name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
         />
+        <meta name="theme-color" content="#317EFB" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>Zooma Deluxe</title>
 
